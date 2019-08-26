0f00cee400db876c4a6e522cfc1df35593ff713e
Sovello Hildebrand <sovellohpmgani@gmail.com>
2019-08-26 17:07:36 -0400

    well, this works, the window won't hang, but crashes after running one command, need to investigate how threads work with QT

--- command_runner.py
+++ command_runner.py
@@ -1,20 +1,21 @@
 import sys
 import os
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QLabel, QLineEdit, QGridLayout
+from PyQt5.QtCore import QTimer
 import subprocess
-
-
+from threading import Thread
+from time import sleep
 
 class CommandRunner(QWidget):
 
     def __init__(self):
         super().__init__()
         # create widgets that we need
-        self.target_label = QLabel("Target Address", self)
-        self.target_entry = QLineEdit()
-
         self.command_label = QLabel("Command", self)
         self.command_entry = QLineEdit()
+
+        self.target_label = QLabel("Parameters", self)
+        self.target_entry = QLineEdit()
 
         self.output_label = QLabel("Output", self)
         self.output_area = QTextEdit()
@@ -26,17 +27,17 @@ class CommandRunner(QWidget):
         layout = QGridLayout()
 
         # weka widgets kwenye layout
-        layout.addWidget(self.target_label, 0, 0)
-        layout.addWidget(self.target_entry, 0, 1)
-        layout.addWidget(self.command_label, 1, 0)
-        layout.addWidget(self.command_entry, 1, 1)
+        layout.addWidget(self.command_label, 0, 0)
+        layout.addWidget(self.command_entry, 0, 1)
+        layout.addWidget(self.target_label, 1, 0)
+        layout.addWidget(self.target_entry, 1, 1)
         layout.addWidget(self.output_label, 2, 0)
         layout.addWidget(self.run_button, 2, 1)
         layout.addWidget(self.output_area, 3, 0, 1, 2)
 
         # set layout kwa main widget
         self.setLayout(layout)
-
+            
         self.setGeometry(600, 600, 300, 150)
         self.setWindowTitle('Command Runner')    
         self.show()
@@ -46,24 +47,25 @@ class CommandRunner(QWidget):
         Gets the target address and the command and creates a command to run on the
         target address
         '''
-        target = self.target_entry.text()
+        target = self.target_entry.text().split(' ')
         command = self.command_entry.text()
-        # read command output into a variable. this is synchronous.
-        # may need to research how to read asynchronous output.
-        # output = os.popen(f'{command} {target}').read()
         args = [command]
         if target:
-            args.append(target)
+            for a in target:
+                args.append(a)
         self.output_area.clear()
+        thread =Thread(target=self.get_command_output, args=(args,), daemon=True)
+        thread.start()
+
+    def get_command_output(self, args):
         with subprocess.Popen(args, bufsize=1, stdout=subprocess.PIPE, universal_newlines=True) as proc:
             while True:
                 output = proc.stdout.readline()
-                # self.output_area.append("Sovello")
                 if output == '' and proc.poll() is not None:
                     break
                 if output:
-                    self.output_area.setText(output.strip())
-                    print(output.strip())
+                    self.output_area.append(output.strip())
+                QTimer().start(1000)
 
 if __name__ == '__main__':
     qapp = QApplication(sys.argv)
